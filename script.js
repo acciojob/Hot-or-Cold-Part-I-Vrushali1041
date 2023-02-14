198f3e3abfd40af083290a995811bb2297607373
Vrushali Patil <113831357+Vrushali1041@users.noreply.github.com>
2023-02-14 10:29:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,20 +1,39 @@
 //your code here
-var randomNum;
-var input;
-function randomNumGenerator(){
-	randomNum =Math.floor(Math.random()*41) - 20;
-	document.getElementById("num").innerHTML = randomNum;
-	guessName();
-	print();
-}
-function guessName(){
-	input = parsInt(document.getElementById("guess").value);
-}
-function print(){
-	if(Math.abs(randomNum-input) <= 5){
-		document.getElementById("respond").innerHTML="Hot";
+// var randomNum;
+// var input;
+// function randomNumGenerator(){
+// 	randomNum =Math.floor(Math.random()*41) - 20;
+// 	document.getElementById("num").innerHTML = randomNum;
+// 	guessName();
+// 	print();
+// }
+// function guessName(){
+// 	input = parsInt(document.getElementById("guess").value);
+// }
+// function print(){
+// 	if(Math.abs(randomNum-input) <= 5){
+// 		document.getElementById("respond").innerHTML="Hot";
+// 	}
+// 	else{
+// 		document.getElementById("respond").innerHTML="Cold";
+// 	}
+// }
+
+
+var btn=document.getElementById('btn');
+var p=document.getElementById('num');
+
+var res=document.getElementById('respond');
+var inp=document.getElementById('guess').value;
+
+function f() {
+	let no=Math.floor(Math.random()*20);
+	p.textContent=no;
+	var inp=document.getElementById('guess').value;
+    if(Number(inp)-no<=5){
+		res.textContent="Hot";
+	}else{
+		res.textContent="Cold";
 	}
-	else{
-		document.getElementById("respond").innerHTML="Cold";
-	}
 }
+btn.addEventListener("click",f);
